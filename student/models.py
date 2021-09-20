4ae8eff778c565841df22904aa52f56428a8d94c
Vardhan Rajnikant Shah <vardhan.s@ahduni.edu.in>
2021-09-20 18:22:38 +0530

    Subject model created

--- student/models.py
+++ student/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    SubjectNo = models.TextField(unique=True)
+    SubjectName = models.TextField()
+    SubjectCode = models.TextField()
+    SubjectProfessor = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
